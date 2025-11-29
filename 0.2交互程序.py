5a27e89c4498fda9a5fc29c26385e7d670059e83
TomLee-Python <2122575771@qq.com>
2025-11-29 12:59:26 +0800

    test: 验证VSCode推送功能

--- 0.2交互程序.py
+++ 0.2交互程序.py
@@ -1,9 +1,6 @@
+# 测试VS Code的Git推送功能
 your_name = input("请输入名字:")
 print(f"你好！{your_name}")
 print("幸会！",your_name)
 print("="*10)
 
-def mysum(mylst):
-    s=mylst[0]
-    for item in myslt[1:]:
-        return s
